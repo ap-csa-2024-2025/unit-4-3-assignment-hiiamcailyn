7f6275bb8fbb1d29a33e83755e1de660a404ecfd
hiiamcailyn <cailyntran@gmail.com>
2024-11-07 10:00:34 -0800

    gi

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    for (i=1; 1 =< 25; 1 += 2)
+    {
+      System.out.println(i);
+    }
   }
 }
