b2c6745b7061dab381fb8f10f4268a75545aef3e
hiiamcailyn <cailyntran@gmail.com>
2024-11-07 10:05:38 -0800

    hi

--- Main.java
+++ Main.java
@@ -4,9 +4,20 @@ public class Main
 {
   public static void main(String[] args)
   {
-    for (i=1; 1 =< 25; 1 += 2)
+    for (int i = 1; 1 <= 25; 1+=2)
     {
       System.out.println(i);
     }
   }
+ 
+  for (int i = 17; i <= 73; i++)
+  {
+    System.out.print(i + " ");
+
+   if (i % 10 == 6)
+   {
+      System.out.println();
+    }
+  }
+
 }
